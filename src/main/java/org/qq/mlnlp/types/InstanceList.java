ec87a878a3115cd3d5a08c7bd14906eba1c5d9c9
emma-cai <qingqing.cai@temple.edu>
2015-07-20 09:42:16 -0400

    add new files

---
+++ src/main/java/org/qq/mlnlp/types/InstanceList.java
@@ -0,0 +1,11 @@
+package org.qq.mlnlp.types;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by qingqingcai on 7/17/15.
+ */
+public class InstanceList extends ArrayList<Instance> implements Serializable {
+
+}
